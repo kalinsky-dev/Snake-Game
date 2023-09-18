244dc7e105d135b163254105b3050d52615deab9
Kalin Hristov <kalin0169@gmail.com>
2023-09-18 10:33:05 +0300

    Add JSDoc to receive an intellisense im my app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const canvas = document.getElementById('canvas');
+
+// add JSDoc to receive intellisense
+/**@type  {CanvasRenderingContext2D}*/
+
 const ctx = canvas.getContext('2d');
 
 window.ctx = ctx;
+
+ctx.beginPath();
